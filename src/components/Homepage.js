694a5969886caf48821a11ee8fc9a9dd3b9593fa
austinloveless <austinloveless5171@gmail.com>
2018-08-22 14:42:11 -0600

    fully functional authenicated messing warbler

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,25 +3,25 @@ import { Link } from "react-router-dom";
 import MessageTimeline from "./MessageTimeline";
 
 const Homepage = ({ currentUser }) => {
-    if (!currentUser.isAuthenticated) {
-        return (
-            <div className="home-hero">
-                <h1>Hi working</h1>
-                <h4>New to Warbler</h4>
-                <Link to="/signup" className="btn btn-primary">
-                    Sign Up here!
-                </Link>
-            </div>
-        );
-    }
+  if (!currentUser.isAuthenticated) {
     return (
-        <div>
-            <MessageTimeline
-                profileImage={currentUser.user.profileImage}
-                username={currentUser.user.username}
-            />
-        </div>
+      <div className="home-hero">
+        <h1>Welcome!</h1>
+        <h4>New to Warbler?</h4>
+        <Link to="/signup" className="btn btn-primary">
+          Sign Up here!
+        </Link>
+      </div>
     );
+  }
+  return (
+    <div>
+      <MessageTimeline
+        profileImage={currentUser.user.profileImage}
+        username={currentUser.user.username}
+      />
+    </div>
+  );
 };
 
 export default Homepage;
